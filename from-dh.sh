aef5ad41bfb713877559f9d1876dd62afddd3310
Chris Dzombak <chris@chrisdzombak.net>
2012-03-06 14:19:50 -0500

    add some quick deployment scripts

---
+++ from-dh.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rsync -vz chrisdz@cdzombak.net:/home/chrisdz/web/colab.cdzombak.net/ colab/
---
+++ to-dh.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rsync -vz colab/ chrisdz@cdzombak.net:/home/chrisdz/web/colab.cdzombak.net/
