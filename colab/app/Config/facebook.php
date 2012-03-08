c61be5c6ac582b32ab7ff86d4a323522085239cc
Chris Dzombak <chris@chrisdzombak.net>
2012-03-07 22:32:08 -0500

    add FB plugin for CakePHP 2.0+

---
+++ colab/app/Config/facebook.php
@@ -0,0 +1,10 @@
+<?php
+$config = array(
+  'Facebook' => array(
+  	'appId' => '322117877837919',
+    'apiKey' => '322117877837919',
+    'secret' => '7b410e14c1bd6ae2bfe64ff4259efa33',
+    'cookie' => true,
+    'locale' => 'en_US',
+  )
+);
