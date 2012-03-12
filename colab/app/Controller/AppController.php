0cd7452d14ea1ec0ac31787a24bf465b1c70049d
Chris Dzombak <chris@chrisdzombak.net>
2012-03-12 00:14:13 -0400

    basic user creation/login works now

--- colab/app/Controller/AppController.php
+++ colab/app/Controller/AppController.php
@@ -5,8 +5,7 @@ class AppController extends Controller {
 	public $helpers = array('Form', 'Html', 'Js', 'Time', 'Session', 'Facebook.Facebook');
 
 	public function afterFacebookLogin() {
-		// i just want this to fucking get called for fucks sake. the rest of it doesn't even have to work.
-		file_put_contents("/home/chrisdz/web/colab.cdzombak.net/app/tmp/callback", "");
+		//file_put_contents("/home/chrisdz/web/colab.cdzombak.net/app/tmp/callback", "");
 		
 		$id = $this->Connect->user('id');
 		$name = $this->Connect->user('name');
@@ -16,16 +15,17 @@ class AppController extends Controller {
 			'conditions' => array('User.facebook_id' => $id)
 		));
 		
-		file_put_contents("/home/chrisdz/web/colab.cdzombak.net/app/tmp/user", $user);
+		//file_put_contents("/home/chrisdz/web/colab.cdzombak.net/app/tmp/user", $user);
 		
 		// if not, create the model & save it
 		
 		
+		// TODO finish this name-saving code once we start work on FB auth
 		// TODO handle updating name if it has changed
 	}
 	
 	public function isAuthorized($user) {
-		if (isset($user['id'])) {
+		if (isset($user['role']) && $user['role'] === 'admin') {
 			return true;
 		}
 		return false;
